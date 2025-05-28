e7213b2292e8e70ea0611d7fe1514bcd3f0b5ce9
NFANSU O BARROW <nfansubarrow300@gmail.com>
2025-05-28 06:53:06 +0100

    Developping a class of led

--- Phase1-Basics/LED_BLINK/LED_BLINK.py
+++ Phase1-Basics/LED_BLINK/LED_BLINK.py
@@ -1,1 +1,34 @@
 import machine
+import time
+
+class LED_CONTROLLER:
+    
+    #Constructor method
+    def __init__(self, pin_number, active_state = True):
+        self.led = machine.Pin(pin_number, machine.Pin.OUT)
+        self.active = active_state
+        
+    #method for switching on led 
+    def led_on(self):
+        self.led.value(1)
+    
+    #method to turn off led
+    def led_off(self):
+        self.led.value(0)
+    
+    #method to toggle led state
+    def led_toggle(self):
+        self.led.value(not self.led.value())
+        
+    #blinking led n times
+    def blinkingLed(self, number_of_times, on_delay, off_delay):
+        for i in range(number_of_times):
+            self.led.on()
+            time.sleep(on_delay)
+            self.led.off()
+            time.sleep(off_delay)
+        print("End of blinking ✌")
+
+myLed = LED(2)
+
+        
