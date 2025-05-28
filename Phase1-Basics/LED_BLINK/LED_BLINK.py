cae3014d333ca36c2db440945fb6894f44f3032e
NFANSU O BARROW <nfansubarrow300@gmail.com>
2025-05-28 08:06:13 +0100

    Controlling LED as a traffic light

--- Phase1-Basics/LED_BLINK/LED_BLINK.py
+++ Phase1-Basics/LED_BLINK/LED_BLINK.py
@@ -20,6 +20,9 @@ class LED_CONTROLLER:
     def led_toggle(self):
         self.led.value(not self.led.value())
         
+    def value(self):
+        return self.led.value()
+        
     #blinking led n times
     def blinkingLed(self, number_of_times, on_delay, off_delay):
         for i in range(number_of_times):
@@ -28,7 +31,45 @@ class LED_CONTROLLER:
             self.led.off()
             time.sleep(off_delay)
         print("End of blinking ✌")
+        
+    def Blink(self, on_delay, off_delay):
+        while True:
+            self.led.value(1)
+            time.sleep(on_delay)
+            self.led.value(0)
+            time.sleep(off_delay)
+        
+        
+# am using 4, 2, 15
 
-myLed = LED(2)
+
+def traffic_light(redPin, bluePin, yellowPin):
+    #instantiating led objects
+    redLight = LED_CONTROLLER(redPin)
+    blueLight = LED_CONTROLLER(bluePin)
+    yellowLight = LED_CONTROLLER(yellowPin)
+    
+    while True:
+        redLight.led_on()
+        time.sleep(1)
+        redLight.led_off()
+        
+        blueLight.led_on()
+        time.sleep(1)
+        blueLight.led_off()
+        
+        yellowLight.led_on()
+        time.sleep(1)
+        yellowLight.led_off()
+        
+
+#traffic_light(redPin=2, bluePin=15, yellowPin=4)
+
+
+        
+        
+    
+    
+
 
         
